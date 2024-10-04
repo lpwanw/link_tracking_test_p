e2e375f771080d6118d25d7bc0e3912331a094b7
tayle-phuong <tay.le@webmd.net>
2024-10-04 18:19:13 +0700

    Remove Unuse test

--- test/controllers/link_clicks_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class LinkClicksControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get link_clicks_create_url
-    assert_response :success
-  end
-end
